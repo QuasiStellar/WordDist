471a34bde65ce78634f4c1d4526b6f54f6287894
QuasiStellar <quasistellar.official@gmail.com>
2021-10-01 20:02:20 +0300

    final version (?!)

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygtrie
 import networkx
-import matplotlib.pyplot as plt
-from networkx_viewer import Viewer
 
 ALPHABET = "alphabet_rus.txt"
 WORD_LIST = "word_rus.txt"
@@ -33,6 +31,10 @@ for word in words:
     for neighbour in neighbours(word):
         if words.has_node(neighbour):
             graph.append((word[:-1], neighbour[:-1]))
+
+G = networkx.Graph()
+G.add_edges_from(graph)
+print([p for p in networkx.all_shortest_paths(G,source='кот',target='ель')])
 
 with open('graph.txt', 'w', encoding="utf-8") as file:
     for edge in graph:
